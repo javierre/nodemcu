f5fff7d185be0c9753df5128e9f43898f867f627
Javier Perez <javierperezlara@gmail.com>
2019-05-02 11:03:25 +0200

    Sensors.

---
+++ sensors/dht11/dht11.ino
@@ -0,0 +1,36 @@
+﻿#include "DHT.h"
+#define DHTPIN D4
+#define DHTTYPE DHT11  
+
+DHT dht(DHTPIN, DHTTYPE); 
+
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("DHT11 test!");
+  dht.begin();
+}
+ 
+void loop() 
+{
+  // Wait a few seconds between measurements.
+  delay(2000); 
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true); 
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) 
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.println(" *F"); 
+}
+
