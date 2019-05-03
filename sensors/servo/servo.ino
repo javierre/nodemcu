a844e45eee9c5aef213e3308894c3ac22c1499a2
Javier Perez <javierperezlara@gmail.com>
2019-05-04 00:33:14 +0200

    Servo

---
+++ sensors/servo/servo.ino
@@ -0,0 +1,36 @@
+/*
+  Creado: Luis del Valle (ldelvalleh@programarfacil.com)
+  https://programarfacil.com
+*/
+ 
+// Incluímos la librería para poder controlar el servo
+#include <Servo.h>
+ 
+// Declaramos la variable para controlar el servo
+Servo servoMotor;
+ 
+void setup() {
+  // Iniciamos el monitor serie para mostrar el resultado
+  Serial.begin(9600);
+ 
+  // Iniciamos el servo para que empiece a trabajar con el pin D7
+  servoMotor.attach(D7);
+}
+ 
+void loop() {
+  
+  // Desplazamos a la posición 0º
+  servoMotor.write(0);
+  // Esperamos 1 segundo
+  delay(1000);
+  
+  // Desplazamos a la posición 90º
+  servoMotor.write(90);
+  // Esperamos 1 segundo
+  delay(1000);
+  
+  // Desplazamos a la posición 180º
+  servoMotor.write(180);
+  // Esperamos 1 segundo
+  delay(1000);
+}
