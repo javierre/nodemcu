6aa0300aed2e6961a6dea1a69b05cee8d94aeaf3
Javier Perez <javierperezlara@gmail.com>
2019-05-06 16:42:54 +0200

    WiFi AP & STA

---
+++ WiFi/nodemcu_AP_STA/nodemcu_AP_STA.ino
@@ -0,0 +1,49 @@
+#include <ESP8266WiFi.h> 
+#include <ESP8266WebServer.h>
+
+
+const char *ssid_AP = "ESP8266_AP"; 
+const char *password_AP = "12345678";
+
+const char *ssid_STA = "mi_ssid_local"; 
+const char *password_STA = "????????"; 
+ESP8266WebServer server(80);
+
+void setup() { 
+  Serial.begin(115200);
+  delay(10); 
+  Serial.println();
+
+  WiFi.mode(WIFI_AP_STA); 
+  WiFi.softAP(ssid_AP, password_AP); 
+  WiFi.begin(ssid_STA, password_STA); 
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500); 
+    Serial.print(".");
+  }
+
+  WiFi.setAutoReconnect(true); 
+  Serial.println("WiFi conectada.");
+
+  Serial.println(); 
+  WiFi.printDiag(Serial); 
+  Serial.println();
+  
+  Serial.print("AP dirección IP: "); 
+  Serial.println(WiFi.softAPIP()); 
+  Serial.print("STA dirección IP: "); 
+  Serial.println(WiFi.localIP());
+
+  server.on("/", [](){
+    server.send(200, "text/plain", "Hola Mundo!!");
+  });
+  server.begin(); 
+  Serial.println("Servidor inicializado.");
+}
+
+void loop() { 
+  server.handleClient();
+}
+
+
