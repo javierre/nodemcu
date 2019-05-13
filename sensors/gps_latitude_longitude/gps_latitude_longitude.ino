b1ec7144ea2cff3c9d0b61d07274a7f7803ba056
Javier Perez <javierperezlara@gmail.com>
2019-05-13 10:14:35 +0200

    GPS NEO 6M Codes

---
+++ sensors/gps_latitude_longitude/gps_latitude_longitude.ino
@@ -0,0 +1,35 @@
+/*
+ * Rui Santos 
+ * Complete Project Details http://randomnerdtutorials.com
+ */
+ 
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+static const int RXPin = D6, TXPin = D5;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+void setup(){
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
+}
+
+void loop(){
+  // This sketch displays information every time a new sentence is correctly encoded.
+  //Serial.println("hh");
+  while (ss.available() > 0){
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()){
+      Serial.print("Latitude= "); 
+      Serial.print(gps.location.lat(), 6);
+      Serial.print(" Longitude= "); 
+      Serial.println(gps.location.lng(), 6);
+    }
+  }
+}
