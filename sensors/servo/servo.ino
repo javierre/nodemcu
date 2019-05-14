11b39720f1105d36edc702091b884d6aa9c575b8
Javier Perez <javierperezlara@gmail.com>
2019-05-14 18:32:44 +0200

    Servo SG90

--- sensors/servo/servo.ino
+++ sensors/servo/servo.ino
@@ -13,7 +13,7 @@ void setup() {
   // Iniciamos el monitor serie para mostrar el resultado
   Serial.begin(9600);
  
-  // Iniciamos el servo para que empiece a trabajar con el pin D7
+  // Iniciamos el servo para que empiece a trabajar con el pin D0
   servoMotor.attach(D0);
 }
  
