c51d12302ae1fd6b08f55a78b15f28f6133849d1
Javier Perez <javierperezlara@gmail.com>
2019-05-14 18:27:36 +0200

    Servo Paso a Paso

---
+++ sensors/servo_paso_a_paso/servo_paso_a_paso.ino
@@ -0,0 +1,43 @@
+/*
+  Creado: Luis del Valle (ldelvalleh@programarfacil.com)
+  https://programarfacil.com
+*/
+
+// Incluímos la librería para poder controlar el servo
+#include <Servo.h>
+
+// Declaramos la variable para controlar el servo
+Servo servoMotor;
+
+void setup() {
+  // Iniciamos el monitor serie para mostrar el resultado
+  Serial.begin(9600);
+
+  // Iniciamos el servo para que empiece a trabajar con el pin 9
+  servoMotor.attach(D0);
+
+  // Inicializamos al ángulo 0 el servomotor
+  servoMotor.write(0);
+}
+
+void loop() {
+
+  // Vamos a tener dos bucles uno para mover en sentido positivo y otro en sentido negativo
+  // Para el sentido positivo
+  for (int i = 0; i <= 90; i++)
+  {
+    // Desplazamos al ángulo correspondiente
+    servoMotor.write(i);
+    // Hacemos una pausa de 25ms
+    delay(25);
+  }
+
+  // Para el sentido negativo
+  for (int i = 89; i > 0; i--)
+  {
+    // Desplazamos al ángulo correspondiente
+    servoMotor.write(i);
+    // Hacemos una pausa de 25ms
+    delay(25);
+  }
+}
