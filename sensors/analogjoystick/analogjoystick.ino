1c37aa553d588489bd1c614af0bce9564ae20479
Javier Perez <javierperezlara@gmail.com>
2019-05-15 11:01:27 +0200

    Analog Joystick

--- sensors/analogjoystick/analogjoystick.ino
+++ sensors/analogjoystick/analogjoystick.ino
@@ -1,13 +1,20 @@
 
 
+#define X_axis A0
+#define ButtonP D6
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(ButtonP, INPUT_PULLUP);
+  pinMode(X_axis, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int value=analogRead(A0);
+  int value=analogRead(X_axis);
   Serial.println(value);
+  
+  if(digitalRead(ButtonP)==LOW) Serial.println("Button Pressed!");
 
 }
